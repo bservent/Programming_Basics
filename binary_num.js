7198794abd0fdd09f917ad682dae461a0383e744
bservent <bservent@gmail.com>
2022-04-12 12:11:55 -0700

    binary num notes

---
+++ binary_num.js
@@ -0,0 +1,13 @@
+/* BINARY NUM INTRO */
+
+/* We use deca system in daily life but computers use binary system
+deca system is based on number 1-9 but binary is based on 0 & 1
+deca = 10^3(thousandths), 10^2 (hundredths), ...etc 
+binary = 2^3, 2^2, 2^1, 2^0 */
+
+/* BINARY DECA NUMBER CONVERSION */
+
+/* binary to decimal = 10110011 = 128 + 0 + 32 + 16 + 0 + 0 + 2 + 1 = 179 
+00111001 = 32 + 16 + 8 + 1 = 57 
+decimal to binary = 296 = 512(0), 256(1), 128(0), 64(0), 32(1), 16(0), 8(1), 4(0), 2(0), 1(0) = 0100101000*/
+
