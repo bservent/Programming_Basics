2877ac4ca43429b2b71e4ab8ca3dd455f9d02517
bservent <bservent@gmail.com>
2022-04-13 17:42:19 -0700

    n and o notation

---
+++ algorithms.js
@@ -0,0 +1,32 @@
+/* ANALYZING ALGORITHMS */
+
+/* INTRO TO TIME COMPLEXITY */
+
+/* Time complexity is a standard way of analyzing and comparing different algorithms */
+
+/* n-NOTATION */
+
+/* n is a function of n
+output is a function of input
+ex: algorithm takes in n num of fb friends and outputs n^2 (inefficient function)
+What's the point of n?
+  -allows us to see how an algorithm scales with input size
+    -if we design an algorithm, we want to know, we want to know, is it efficient with more people and input? 
+    -We want our programs to succeed, so we want to make solutions which scale.
+    -is the algorithm scalable */
+
+/* n-NOTATION SCALING GOALS */
+
+/* Goals
+-We are looking for large patterns
+-We want to see major efficiency changes btw diff algorithms 
+
+Rules
+-We don't care about multiples ex: n^2 = 2n^2
+-We take the largest in an equation*/
+
+/* BIG O NOTATION */
+
+/* big O represents a boundry
+a program is dictated by it's worst comoponent 
+big O represents equal to or faster when consider in an algorithm */
